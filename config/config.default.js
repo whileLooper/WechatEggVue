c77c472e569d718fb56d9e6941af206ff2d81979
b004577 <bo.chen@macys.com>
2018-02-16 15:05:11 -0500

    switch back to testing config

--- config/config.default.js
+++ config/config.default.js
@@ -33,10 +33,10 @@ module.exports = app => {
   // wechat configuration
   exports.initWechat = {
     token: 'bhu89ijnmko0',
-    appID: 'wxd61026a80b610e7c',
-    appSecret: '7b9dde29e00e60d9474551e06e409c3f587a45fca820916e183acca9e41839e',
-    // appID: 'wx58080b1fe9cb4f49',
-    // appSecret: '9dba811200ddd73da9a24d7f36762485',
+    // appID: 'wxd61026a80b610e7c',
+    // appSecret: '7b9dde29e00e60d9474551e06e409c3f587a45fca820916e183acca9e41839e',
+    appID: 'wx58080b1fe9cb4f49',
+    appSecret: '9dba811200ddd73da9a24d7f36762485',
     encodingAESKey: 'HX2DzbFRh9qY4toNv9XVL1kYxgoYCTwoZ3haN92kAzl',
     getAccessToken() {
       return util.readFileAsync(wechat_file);
